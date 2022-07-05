a2a4fb114b5a4d5f958dc2d6310eedd2402562a7
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-07-05 23:23:14 +0300

    mvc

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require("express");
+const tasks = require("./routes/tasks");
 
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 app.get("/hello", (req, res) => {
   res.send("Task Manager App");
 });
+
+app.use("/api/v1/tasks", tasks);
 
 app.listen(PORT, () =>
   console.log(`Server is listening on http://localhost:${PORT}`)
