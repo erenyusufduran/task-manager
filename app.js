f861bf63e07a4d3cbc1810962be951dca91f8b0c
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-07-06 02:30:39 +0300

    not found page

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
-require('dotenv').config();
+const notFound = require("./middleware/not-found");
+require("dotenv").config();
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 app.use("/api/v1/tasks", tasks);
+
+app.use(notFound);
 
 const startServer = async () => {
   try {
