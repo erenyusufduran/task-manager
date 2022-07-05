f51316cc81fb1de9535487a2383ed80c1dd77cd8
Eren Yusuf Duran <erenyusufduran1905@gmail.com>
2022-07-05 23:05:38 +0300

    public folder and server setups

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/hello", (req, res) => {
+  res.send("Task Manager App");
+});
+
+app.listen(PORT, () =>
+  console.log(`Server is listening on http://localhost:${PORT}`)
+);
